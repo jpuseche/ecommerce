1eda5ad2167bb916d81d12bf8a82c1edfc2982e4
[juseche] <[juseche@motus.com]>
2024-07-23 17:34:19 -0500

    feat(mongodb setup): DBSetup and custom errors

--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,29 @@
 package database
+
+import "errors"
+
+var (
+	ErrCantFindProduct    = errors.New("couldn't find the Product")
+	ErrCantDecodeProducts = errors.New("couldn't find the Product")
+	ErrUserIdIsNotValid   = errors.New("this User is not valid")
+	ErrCantUpdateUser     = errors.New("can't add the Product to the Cart")
+	ErrCantRemoveItemCart = errors.New("can't remove this Item from Cart")
+	ErrCantGetItem        = errors.New("can't get Item from Cart")
+	ErrCantBuyCartItem    = errors.New("can't update Purchase")
+)
+
+func AddProductToCart() {
+
+}
+
+func RemoveCartItem() {
+
+}
+
+func BuyItemFromCart() {
+
+}
+
+func InstantBuyer() {
+
+}
